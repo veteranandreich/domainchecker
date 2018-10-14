25a72d4bfeb65009f9411eacebe22bbfc6fcdc16
Vladimir <vladimir27goncharov@icloud.com>
2018-10-14 13:49:56 +0300

    Release 1.0

---
+++ domaincheck/domainchecker.py
@@ -0,0 +1,23 @@
+import urllib.request
+from bs4 import BeautifulSoup
+def get_html(url):
+    response = urllib.request.urlopen(url)
+    return response.read()
+
+def parse(html,site):
+    soup=BeautifulSoup(html,"html.parser")
+    answer=soup.find('div', class_="_1_uOq")
+    if answer is None:
+        print('{}свободен'.format(site[0:-1]))
+    else:
+        print(answer.text)
+
+
+def main():
+    name=input('Enter your wished name ')
+    f=open('domainlist.txt','r')
+    for str in f:
+        parse(get_html('https://www.nic.ru/whois/?searchWord='+name+str),name+str)
+
+if __name__ == '__main__':
+    main()
